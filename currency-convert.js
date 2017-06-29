9b7a2cf4d12d22ed8e1a7f3070ce7c4fe04b405e
Shock Liang <shockwater@gmail.com>
2017-06-29 22:15:28 +0800

    Using promise style to get currency rate and convert it

---
+++ currency-convert.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+const getExchangeRate = (from, to) => {
+    return axios.get(`http://api.fixer.io/latest?base=${from}`).then((response) => {
+        return response.data.rates[to];
+    });
+};
+
+const getCountries = (currencyCode) => {
+    return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {
+        return response.data.map((country) => country.name);
+    });
+};
+
+const convertCurrency = (from, to, amount) => {
+    let countries;
+    return getCountries(to).then((tempCountries) => {
+        countries = tempCountries;
+        return getExchangeRate(from, to);
+    }).then((rate) => {
+        const exchangedAmount = amount * rate;
+        return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries:${countries.join(', ')}`
+    });
+};
+
+convertCurrency('USD', 'CAD', 100).then((status) => {
+    console.log(status);
+})
+
+// getCountries('CAD').then((countries) => {
+//     console.log(countries);
+// });
+
+// getExchangeRate('USD', 'EUR').then((rate) => {
+//     console.log(`rate:${rate}`);
+// });
