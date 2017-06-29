de17105b8cf4d60b49829e490420ba270751caa8
Shock Liang <shockwater@gmail.com>
2017-06-29 21:29:35 +0800

    using async/await to accomplish same thing

--- app-promises.js
+++ app-promises.js
@@ -56,11 +56,28 @@ const getStatus = (userId) => {
     });
 };
 
-getStatus(1).then((status) => {
+const getStatusAlt = async (userId) => {
+    const user = await getUser(userId);
+    const grades = await getGrades(user.schoolId);
+    let average = 0; 
+    if(grades.length > 0) {
+        average = grades.map((grade) => grade.grade).reduce((a, b) => a + b) / grades.length;
+    }
+
+    return `${user.name} has ${average}% in the class.`;
+};
+
+getStatusAlt(1).then((status) => {
     console.log(status);
 }).catch((e) => {
     console.log(e);
 });
+
+// getStatus(1).then((status) => {
+//     console.log(status);
+// }).catch((e) => {
+//     console.log(e);
+// });
 
 // getGrades(999).then((grade) => {
 //     console.log(grade);
